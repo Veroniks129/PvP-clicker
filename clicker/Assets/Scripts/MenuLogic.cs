c71f8162a361c09028497d2894fd5080696d3346
Мартьянов Евгений Михайлович <ev.m05@yandex.ru>
2025-12-19 17:14:24 +0300

    Upgrade menu navigation

--- clicker/Assets/Scripts/MenuLogic.cs
+++ clicker/Assets/Scripts/MenuLogic.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Threading.Tasks;
 using TMPro;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Netcode;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Multiplayer;
-using UnityEditor;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MenuLogic : NetworkBehaviour
@@ -13,6 +15,12 @@ public class MenuLogic : NetworkBehaviour
     [SerializeField] private TMP_InputField join_code_input;
     [SerializeField] private GameObject host_menu;
     [SerializeField] private GameObject common_menu;
+    [SerializeField] private GameObject choice_buttons;
+    [SerializeField] private GameObject exit_button;
+
+    [SerializeField] private TMP_Text players_count_field;
+    private NetworkVariable<int> players_count = new NetworkVariable<int>(0,
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
 
     [SerializeField] private CanvasGroup game_map_cg;
     [SerializeField] private MapInit game_map_mi;
@@ -20,46 +28,80 @@ public class MenuLogic : NetworkBehaviour
     [SerializeField] private GameObject menu;
     [SerializeField] private GameObject map_blocker;
 
+    private ISession session;
+    bool is_host;
+    private NetworkVariable<ulong> host_id = new NetworkVariable<ulong>(0,
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
     public void MenuStart()
     {
         menu.SetActive(true);
+        ChoiceMenuVisibility(true);
         HostMenuVisibility(false);
         ClientMenuVisibility(false);
     }
 
     public void SelectHost()
     {
+        is_host = true;
+        ChoiceMenuVisibility(false);
         ClientMenuVisibility(false);
-        _ = HostGame();
+        HostGame();
         HostMenuVisibility(true);
         game_map_mi.InitializeMap();
     }
 
     public void SelectClient()
     {
+        is_host = false;
+        ChoiceMenuVisibility(false);
         HostMenuVisibility(false);
-        _ = JoinGame(join_code_input.text);
+        JoinGame(join_code_input.text);
         ClientMenuVisibility(true);
     }
 
-    async Task HostGame()
+    public void SelectExit()
+    {
+        Debug.Log($"Selected Exit");
+        HostMenuVisibility(false);
+        ClientMenuVisibility(false);
+        ExitGame();
+        ChoiceMenuVisibility(true);
+        join_code_field.text = "...wait...";
+        players_count_field.text = "...wait...";
+    }
+
+    async System.Threading.Tasks.Task HostGame()
     {
         var options = new SessionOptions
         {
             MaxPlayers = 4
         }.WithRelayNetwork();
 
-        var session = await MultiplayerService.Instance.CreateSessionAsync(options);
+        session = await MultiplayerService.Instance.CreateSessionAsync(options);
         join_code_field.text = session.Code;
 
         NetworkManager.Singleton.StartHost();
     }
 
-    async Task JoinGame(string join_code)
+    async System.Threading.Tasks.Task JoinGame(string join_code)
     {
-        await MultiplayerService.Instance.JoinSessionByCodeAsync(join_code);
+        session = await MultiplayerService.Instance.JoinSessionByCodeAsync(join_code);
+        join_code_field.text = session.Code;
         NetworkManager.Singleton.StartClient();
     }
+
+    async System.Threading.Tasks.Task ExitGame()
+    {
+        if (NetworkManager.Singleton != null)
+        {
+            NetworkManager.Singleton.Shutdown();
+        }
+
+        await session.LeaveAsync();
+        session = null;
+    }
+
 
     void HostMenuVisibility(bool is_active)
     {
@@ -70,6 +112,12 @@ public class MenuLogic : NetworkBehaviour
     void ClientMenuVisibility(bool is_active)
     {
         common_menu.SetActive(is_active);
+    }
+
+    void ChoiceMenuVisibility(bool is_active)
+    {
+        choice_buttons.SetActive(is_active);
+        exit_button.SetActive(!is_active);
     }
     
     public void TriggerStart()
@@ -87,5 +135,47 @@ public class MenuLogic : NetworkBehaviour
         map_blocker.SetActive(false);
         game_paths.SetActive(true);
         menu.SetActive(false);
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            host_id.Value = NetworkManager.Singleton.LocalClientId;
+
+            NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedHost;
+            NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnectedHost;
+        } else
+        {
+            NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnectedLocal;
+        }
+
+        host_id.OnValueChanged += (_, newValue) => Debug.Log($"Host ID received: {newValue}");
+
+        players_count.OnValueChanged += (_, newValue) =>
+        {
+            players_count_field.text = $"{newValue}/4";
+        };
+    }
+
+    private void OnClientConnectedHost(ulong _)
+    {
+        players_count.Value = NetworkManager.Singleton.ConnectedClients.Count;
+        host_id.Value = NetworkManager.Singleton.LocalClientId;
+    }
+
+    private void OnClientDisconnectedHost(ulong _)
+    {
+        players_count.Value = NetworkManager.Singleton.ConnectedClients.Count;
+    }
+    
+    // Тут должен был быть выход клиента при выходе хоста, но мне никак не удалось заставить его работать
+    private void OnClientDisconnectedLocal(ulong disconnected_id)
+    {
+        Debug.Log($"and so I am here: disc_id: {disconnected_id}, host_id.Value: {host_id.Value}, is_host: {is_host}");
+        if (!IsOwner && !NetworkManager.Singleton.IsConnectedClient || NetworkManager.Singleton.ShutdownInProgress))
+        {
+            SelectExit();
+        }
     }
 }
